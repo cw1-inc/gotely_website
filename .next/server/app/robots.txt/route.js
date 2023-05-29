d84124260af55da3c8f22a13a3f6c9d869f9f54a
Pedro Specter <pedro.specter@cw1.com>
2023-05-29 14:40:08 +0200

    dd

---
+++ .next/server/app/robots.txt/route.js
@@ -0,0 +1,156 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 703;
+exports.ids = [703];
+exports.modules = {
+
+/***/ 7783:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/@edge-runtime/cookies");
+
+/***/ }),
+
+/***/ 8530:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/@opentelemetry/api");
+
+/***/ }),
+
+/***/ 4426:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/chalk");
+
+/***/ }),
+
+/***/ 252:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/cookie");
+
+/***/ }),
+
+/***/ 2196:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/ua-parser-js");
+
+/***/ }),
+
+/***/ 4021:
+/***/ ((module) => {
+
+module.exports = import("next/dist/compiled/@vercel/og/index.node.js");;
+
+/***/ }),
+
+/***/ 5439:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "headerHooks": () => (/* binding */ headerHooks),
+  "originalPathname": () => (/* binding */ originalPathname),
+  "requestAsyncStorage": () => (/* binding */ requestAsyncStorage),
+  "routeModule": () => (/* binding */ routeModule),
+  "serverHooks": () => (/* binding */ serverHooks),
+  "staticGenerationAsyncStorage": () => (/* binding */ staticGenerationAsyncStorage),
+  "staticGenerationBailout": () => (/* binding */ staticGenerationBailout)
+});
+
+// NAMESPACE OBJECT: ./node_modules/next/dist/build/webpack/loaders/next-metadata-route-loader.js?page=%2Frobots.txt%2Froute&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js!./app/robots.js?__next_metadata
+var robots_next_metadata_namespaceObject = {};
+__webpack_require__.r(robots_next_metadata_namespaceObject);
+__webpack_require__.d(robots_next_metadata_namespaceObject, {
+  "GET": () => (GET)
+});
+
+// EXTERNAL MODULE: ./node_modules/next/dist/server/node-polyfill-headers.js
+var node_polyfill_headers = __webpack_require__(5387);
+// EXTERNAL MODULE: ./node_modules/next/dist/server/future/route-modules/app-route/module.js
+var app_route_module = __webpack_require__(9267);
+var module_default = /*#__PURE__*/__webpack_require__.n(app_route_module);
+// EXTERNAL MODULE: ./node_modules/next/server.js
+var server = __webpack_require__(4664);
+;// CONCATENATED MODULE: ./app/robots.js
+function robots() {
+    return {
+        rules: {
+            userAgent: "*",
+            allow: "/"
+        },
+        sitemap: "https://acme.com/sitemap.xml"
+    };
+}
+
+// EXTERNAL MODULE: ./node_modules/next/dist/build/webpack/loaders/metadata/resolve-route-data.js
+var resolve_route_data = __webpack_require__(9334);
+;// CONCATENATED MODULE: ./node_modules/next/dist/build/webpack/loaders/next-metadata-route-loader.js?page=%2Frobots.txt%2Froute&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js!./app/robots.js?__next_metadata
+
+
+
+
+const contentType = "text/plain"
+const fileType = "robots"
+
+async function GET() {
+  const data = await robots()
+  const content = (0,resolve_route_data.resolveRouteData)(data, fileType)
+
+  return new server.NextResponse(content, {
+    headers: {
+      'Content-Type': contentType,
+      'Cache-Control': "public, max-age=0, must-revalidate",
+    },
+  })
+}
+
+;// CONCATENATED MODULE: ./node_modules/next/dist/build/webpack/loaders/next-app-loader.js?page=%2Frobots.txt%2Froute&name=app%2Frobots.txt%2Froute&pagePath=private-next-app-dir%2Frobots.js&appDir=%2FUsers%2Fpedrospecter%2FDownloads%2Fwork%2Fgotely%2Fwebsite%2Fapp&appPaths=%2Frobots&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&basePath=&assetPrefix=&nextConfigOutput=&preferredRegion=!
+
+    
+
+    
+
+    
+
+    const routeModule = new (module_default())({
+    userland: robots_next_metadata_namespaceObject,
+    pathname: "/robots.txt",
+    resolvedPagePath: "next-metadata-route-loader?page=%2Frobots.txt%2Froute&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js!/Users/pedrospecter/Downloads/work/gotely/website/app/robots.js?__next_metadata",
+    nextConfigOutput: undefined,
+  })
+
+    // Pull out the exports that we need to expose from the module. This should
+    // be eliminated when we've moved the other routes to the new format. These
+    // are used to hook into the route.
+    const {
+      requestAsyncStorage,
+      staticGenerationAsyncStorage,
+      serverHooks,
+      headerHooks,
+      staticGenerationBailout
+    } = routeModule
+
+    const originalPathname = "/robots.txt/route"
+
+    
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [405,572,334], () => (__webpack_exec__(5439)));
+module.exports = __webpack_exports__;
+
+})();
