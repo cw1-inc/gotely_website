1b510b240f416415e6509051c53961afa2bc2de7
Pedro Specter <pedro.specter@cw1.com>
2023-06-18 11:36:02 +0200

    dd

--- app/layout.js
+++ app/layout.js
@@ -25,12 +25,12 @@ export const metadata = {
     authors: ['CW1']
   },
   robots: {
-    index: false,
+    index: true,
     follow: true,
     nocache: true,
     googleBot: {
       index: true,
-      follow: false,
+      follow: true,
       noimageindex: true,
       'max-video-preview': -1,
       'max-image-preview': 'large',
